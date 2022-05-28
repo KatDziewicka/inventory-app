ed3771b49af70273bf82dd82094e773002171ff3
Kasia Dziewicka <kasia.dziewicka@faculty.ai>
2022-05-28 15:07:26 +0100

    Use prettier

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
-import Airtable from 'airtable';
-import express from 'express';
+import Airtable from "airtable";
+import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 
@@ -8,33 +8,43 @@ config();
 const port = process.env.PORT;
 
 Airtable.configure({
-    endpointUrl: process.env.BASE_URL,
-    apiKey: process.env.API_KEY,
+  endpointUrl: process.env.BASE_URL,
+  apiKey: process.env.API_KEY,
 });
 
 const app = express();
 
 app.use(cors());
 
-const base = new Airtable({apiKey: process.env.API_KEY}).base('appP9Sdory1eUNZ2G');
+const base = new Airtable({ apiKey: process.env.API_KEY }).base(
+  "appP9Sdory1eUNZ2G"
+);
 
 app.get("/stock", async (req, res) => {
-    await base('Table 1').select({
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            console.log('Retrieved', record);
+  await base("Table 1")
+    .select({
+      maxRecords: 3,
+      view: "Grid view",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function (record) {
+          console.log("Retrieved", record);
         });
         fetchNextPage();
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-  });
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+});
 
 if (!port) {
   console.log("Missing PORT environment variable. Set it in .env file.");
-};
+}
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
